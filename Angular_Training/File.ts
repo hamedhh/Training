6c4b8bbe52f6084c21e053b70cec0b9dd42655e0
hamedhh <50040096+hamedhh@users.noreply.github.com>
2019-09-04 14:09:00 +0430

    angular define type

--- Angular_Training/File.ts
+++ Angular_Training/File.ts
@@ -1,14 +1,40 @@
-interface   person{
-    firstName:string;
-    lastName:string;
-} 
-class MyClasss{
+//----01----
+// interface   person{
+//     firstName:string;
+//     lastName:string;
+// } 
+// class MyClasss{
     
+// }
+
+// function logPerson(person: person)
+// {
+//     return "Hi ,"+person.firstName +"  " + person.lastName;
+// }
+// let user ={firstName:'Hamed',lastName:'HH'};
+// document.body.innerHTML=logPerson(user);
+
+//----02-----
+var listperson:string[]=["Hamed","Iman"];
+console.log(listperson);
+
+var listGen:Array<string> =["Hamed","Iman"]; 
+
+//لیثتی که یک استرینگ و یک عدد میگیرد به عنوان یک آینم
+var tuple:[string,number];
+tuple=["hamed",27];
+
+enum color{
+    green=5, //پیش فرض از صفر شروع میشود
+    red=6
 }
 
-function logPerson(person: person)
-{
-    return "Hi ,"+person.firstName +"  " + person.lastName;
-}
-let user ={firstName:'Hamed',lastName:'HH'};
-document.body.innerHTML=logPerson(user);
+console.log(color[5])
+console.log(color.green)
+
+var harchi:any;//هرچیزی میشود در این تاایپ ریخت
+
+harchi="asd",
+harchi=123;
+
+var listHarchi:any[]=["hamed",21,true]
--- Angular_Training/file.js
+++ Angular_Training/file.js
@@ -1,10 +1,31 @@
-var MyClasss = /** @class */ (function () {
-    function MyClasss() {
-    }
-    return MyClasss;
-}());
-function logPerson(person) {
-    return "Hi ," + person.firstName + "  " + person.lastName;
-}
-var user = { firstName: 'Hamed', lastName: 'HH' };
-document.body.innerHTML = logPerson(user);
+//----01----
+// interface   person{
+//     firstName:string;
+//     lastName:string;
+// } 
+// class MyClasss{
+// }
+// function logPerson(person: person)
+// {
+//     return "Hi ,"+person.firstName +"  " + person.lastName;
+// }
+// let user ={firstName:'Hamed',lastName:'HH'};
+// document.body.innerHTML=logPerson(user);
+//----02-----
+var listperson = ["Hamed", "Iman"];
+console.log(listperson);
+var listGen = ["Hamed", "Iman"];
+//لیثتی که یک استرینگ و یک عدد میگیرد به عنوان یک آینم
+var tuple;
+tuple = ["hamed", 27];
+var color;
+(function (color) {
+    color[color["green"] = 5] = "green";
+    color[color["red"] = 6] = "red";
+})(color || (color = {}));
+console.log(color[5]);
+console.log(color.green);
+var harchi; //هرچیزی میشود در این تاایپ ریخت
+harchi = "asd",
+    harchi = 123;
+var listHarchi = ["hamed", 21, true];
