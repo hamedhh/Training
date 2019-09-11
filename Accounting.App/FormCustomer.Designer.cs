3176711ef1e2891bc0308191f422e29502f843a3
hamedhh <50040096+hamedhh@users.noreply.github.com>
2019-09-11 13:39:43 +0430

    creatre Angular PackageManager

---
+++ Accounting.App/FormCustomer.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Accounting.App
+{
+    partial class FormCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.btnAddNewCustomer = new System.Windows.Forms.ToolStripButton();
+            this.btnEditCustomer = new System.Windows.Forms.ToolStripButton();
+            this.btnDeleteCustomer = new System.Windows.Forms.ToolStripButton();
+            this.btnUpdateCustomer = new System.Windows.Forms.ToolStripButton();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.btnAddNewCustomer,
+            this.btnEditCustomer,
+            this.btnDeleteCustomer,
+            this.btnUpdateCustomer});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(491, 62);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // btnAddNewCustomer
+            // 
+            this.btnAddNewCustomer.Image = global::Accounting.App.Properties.Resources._1371475930_filenew;
+            this.btnAddNewCustomer.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnAddNewCustomer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnAddNewCustomer.Name = "btnAddNewCustomer";
+            this.btnAddNewCustomer.Size = new System.Drawing.Size(44, 59);
+            this.btnAddNewCustomer.Text = "درج";
+            this.btnAddNewCustomer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnEditCustomer
+            // 
+            this.btnEditCustomer.Image = global::Accounting.App.Properties.Resources._1371475973_document_edit;
+            this.btnEditCustomer.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnEditCustomer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnEditCustomer.Name = "btnEditCustomer";
+            this.btnEditCustomer.Size = new System.Drawing.Size(44, 59);
+            this.btnEditCustomer.Text = "ویرایش";
+            this.btnEditCustomer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnDeleteCustomer
+            // 
+            this.btnDeleteCustomer.Image = global::Accounting.App.Properties.Resources._1371476007_Close_Box_Red;
+            this.btnDeleteCustomer.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnDeleteCustomer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnDeleteCustomer.Name = "btnDeleteCustomer";
+            this.btnDeleteCustomer.Size = new System.Drawing.Size(44, 59);
+            this.btnDeleteCustomer.Text = "حذف";
+            this.btnDeleteCustomer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // btnUpdateCustomer
+            // 
+            this.btnUpdateCustomer.Image = global::Accounting.App.Properties.Resources._1371476342_Refresh;
+            this.btnUpdateCustomer.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.btnUpdateCustomer.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.btnUpdateCustomer.Name = "btnUpdateCustomer";
+            this.btnUpdateCustomer.Size = new System.Drawing.Size(77, 59);
+            this.btnUpdateCustomer.Text = "به روز رسانی";
+            this.btnUpdateCustomer.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            // 
+            // FormCustomer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(491, 375);
+            this.Controls.Add(this.toolStrip1);
+            this.Name = "FormCustomer";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "لیست اشخاص";
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton btnAddNewCustomer;
+        private System.Windows.Forms.ToolStripButton btnEditCustomer;
+        private System.Windows.Forms.ToolStripButton btnDeleteCustomer;
+        private System.Windows.Forms.ToolStripButton btnUpdateCustomer;
+    }
+}
